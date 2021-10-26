0c231aa5f348deb69c0e4897f70170dd97423e29
joluhergo <91272288+joluhergo@users.noreply.github.com>
2021-10-26 17:13:24 +0200

    Add files via upload

---
+++ lab_sql_intro.sql
@@ -0,0 +1,9 @@
+SELECT `title` FROM `sakila`.`film`;
+SELECT `name` AS language FROM `sakila`.`language`;
+SELECT * FROM language; #Checking
+SELECT `store_id` FROM `sakila`.`store`;
+SELECT COUNT(store_id)
+FROM `sakila`.`store`;
+SELECT COUNT(staff_id)
+FROM `sakila`.`staff`;
+SELECT `first_name` FROM `sakila`.`staff`;
